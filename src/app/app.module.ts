19f45342f4f7e26606dd766b033428751ae40b61
Racharlasanthosh <santhoshracharacha456@gmail.com>
2022-10-31 11:09:42 +0530

    form.group.completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { TypecoderComponent } from './typecoder/typecoder.component';
 import { BankDetailsComponent } from './bank-details/bank-details.component';
 import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
@@ -64,7 +65,8 @@ import { CreateAccountComponent } from './create-account/create-account.componen
     TypecoderComponent,
     BankDetailsComponent,
     VehicleDetailsComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
